910645d3f2dc1df1d7e2471e6e8743556d77ea82
tij-ardni18 <76863651+indrajit2003@users.noreply.github.com>
2026-08-15 21:45:25 +0530

    Time: 7 ms (66.92%), Space: 32.2 MB (56.04%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int findTotalFloor(int eggs,int moves){
+    int findTotalFloor(int eggs,int moves,vector<vector<int>>&dp){
         if(eggs==0 or moves==0) return 0;
         if(eggs==1) return moves;
-        return 1+findTotalFloor(eggs-1,moves-1)+findTotalFloor(eggs,moves-1);
+        if(dp[eggs][moves]!=-1) return dp[eggs][moves];
+        return dp[eggs][moves] =  1+findTotalFloor(eggs-1,moves-1,dp)+findTotalFloor(eggs,moves-1,dp);
     }
     int superEggDrop(int k, int n) {
         int moves = 0;
-        while(findTotalFloor(k,moves)<n){
+        vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
+        while(findTotalFloor(k,moves,dp)<n){
             moves++;
         }
         return moves;
