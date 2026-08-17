be35cab4f60436e8a40751bc23bf97ac9a4c5d6e
tij-ardni18 <76863651+indrajit2003@users.noreply.github.com>
2026-08-17 22:03:59 +0530

    Time: 58 ms (69.27%), Space: 62.9 MB (8.84%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        if(n==1) return 0;
+        vector<vector<int>>palindrome(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            palindrome[i][i]=1;
+        }
+        for(int leng=2;leng<=n;leng++){
+            for(int i=0;i<=n-leng;i++){
+                int j = i+leng-1;
+                if(s[i]==s[j] and (leng==2 or palindrome[i+1][j-1])){
+                    palindrome[i][j]=1;
+                }
+                
+            }
+        }
+        vector<int>dp(n,INT_MAX);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(palindrome[i][j]){
+                    if(j==n-1)
+                    dp[i]=0;
+                    else
+                    dp[i] = min(dp[i] , 1+dp[j+1]);
+                }
+            }
+        }
+        return dp[0];
+    } 
+};
