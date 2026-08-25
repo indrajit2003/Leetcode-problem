a15490432af3a1ff4afbeae98d70f80d6e4f5a33
tij-ardni18 <76863651+indrajit2003@users.noreply.github.com>
2026-08-25 12:15:37 +0530

    Time: 22 ms (65.03%), Space: 23.4 MB (91.42%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,52 +10,17 @@
 
 class Solution {
 public:
-    void findpath1(TreeNode* root, TreeNode* p,vector<int>&ans){
-         if(root->val == p->val){
-            ans.push_back(root->val);
-            return;
-        }
-
-        ans.push_back(root->val);
-        if(p->val<root->val)
-        findpath1(root->left,p,ans);
-        else
-        findpath1(root->right,p,ans);
-    }
-    void findpath2(TreeNode* root, TreeNode* q,vector<int>&ans){
-        if(root->val == q->val){
-            ans.push_back(root->val);
-            return;
-        }
-        ans.push_back(root->val);
-        if(q->val<root->val)
-        findpath1(root->left,q,ans);
-        else
-        findpath1(root->right,q,ans);
+    TreeNode* LCA(TreeNode* root,TreeNode* p, TreeNode* q){
+        if(! root) return NULL;
+        if(root->val>p->val and root->val>q->val)
+        return LCA(root->left,p,q);
+        else if(root->val<p->val and root->val<q->val)
+        return LCA(root->right,p,q);
+        else 
+        return root;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root->val==p->val or root->val==q->val) return root;
-        vector<int>ans1;
-        vector<int>ans2;
-        findpath1(root,p,ans1);
-        findpath2(root,q,ans2);
-        if(ans1.size()<ans2.size()){
-            while(ans1.size()!= ans2.size())
-            ans1.push_back(INT_MAX);
-        }
-         if(ans2.size()<ans1.size()){
-            while(ans2.size()!= ans1.size())
-            ans2.push_back(INT_MAX);
-        }
-        TreeNode* temp = new TreeNode(NULL);
-        for(int i=1;i<ans1.size();i++){
-            if(ans1[i]!=ans2[i]){
-            TreeNode* temp1 =new TreeNode (ans1[i-1]);
-            temp = temp1;
-            break;
-            }
-        }
-        return temp;
-        
+        return LCA(root,p,q);
     }
 };
