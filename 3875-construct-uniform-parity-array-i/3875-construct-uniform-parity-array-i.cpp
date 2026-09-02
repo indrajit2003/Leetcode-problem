c2150bbcad0517bff8d1bbdbc8530d6b3f5593dd
tij-ardni18 <76863651+indrajit2003@users.noreply.github.com>
2026-09-02 19:05:35 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        if(n==1) return true;
-        bool flag=0;
-        for(int i=0;i<n;i++){
-           if(nums1[i]%2==0)
-           flag=1;
-        }
-        for(int i=0;i<n;i++){
-           if(nums1[i]%2!=0)
-           flag=1;
-        }
-        if(flag==1) return true;
-
-        return false;
+        return true;
     }
 };
