9960c4d4701e0b3bb3500003c0576ca7ee852d42
tij-ardni18 <76863651+indrajit2003@users.noreply.github.com>
2026-09-06 11:23:33 +0530

    Time: 27 ms (71.17%), Space: 44.4 MB (20.6%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int countWays(int m,int n,string s,string t,vector<vector<int>>& dp){
-        if(n==0) return 1;
-        if(m==0)return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        if(s[m-1]==t[n-1]){
-            return dp[m][n]= countWays(m-1,n-1,s,t,dp) + countWays(m-1,n,s,t,dp);
-        }else{
-            return dp[m][n]= countWays(m-1,n,s,t,dp);
-        }
-    }
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return countWays(m,n,s,t,dp);
+        vector<vector<unsigned long long>>dp(m+1,vector<unsigned long long>(n+1,0));
+        for(int i=0;i<=m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1])
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m][n];
     }
 };
